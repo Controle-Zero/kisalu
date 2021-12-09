57f9dad361d0392960e0647dab7a4da8602b842b
Rafael Willen <rafaelpadre20@gmail.com>
2021-12-10 00:48:27 +0100

    Adicionado BottomSheetModal

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,29 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import {
+  BottomSheetModalProvider,
+  BottomSheetModal,
+} from "@gorhom/bottom-sheet";
 import Fonts from "../styles/fontsConstants";
 import PrimaryButton from "../components/buttons/PrimaryButton";
 import welcomeimage from "../assets/images/logooficial.png";
 import Spacer from "../components/layout/Spacer";
+import CreateAccountTypeModal from "../components/modals/CreateAccountTypeModal";
 export default function Welcome() {
-  const onLogin = () => {
-    console.log("Sergio");
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const onModalShown = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const openLogin = () => {
+    // TODO: Roberto implementar navegação para a tela de login
   };
 
   return (
-    <View style={styles.container}>
-      <Image style={styles.direction} source={welcomeimage} />
-      <Text style={styles.welcome}>BEM VINDO</Text>
-      <Text style={styles.introdu}>
-        Uma app que te ajuda a encontrar e providenciar serviços
-      </Text>
-      <View style={styles.alinharbotao}>
-        <PrimaryButton text="Login" color="#383D3B" onPress={onLogin} />
-        <Spacer width={10} height={0} />
-        <PrimaryButton text="Cadastro" color="#60DBDA" onPress={onLogin} />
+    <BottomSheetModalProvider>
+      <View style={styles.container}>
+        <Image style={styles.direction} source={welcomeimage} />
+        <Text style={styles.welcome}>BEM VINDO</Text>
+        <Text style={styles.introdu}>
+          Uma app que te ajuda a encontrar e providenciar serviços
+        </Text>
+        <View style={styles.alinharbotao}>
+          <PrimaryButton text="Login" color="#383D3B" onPress={openLogin} />
+          <Spacer width={10} height={0} />
+          <PrimaryButton
+            text="Cadastro"
+            color="#60DBDA"
+            onPress={onModalShown}
+          />
+        </View>
+        <CreateAccountTypeModal reference={bottomSheetModalRef} />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </BottomSheetModalProvider>
   );
 }
 
