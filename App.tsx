ccb06866bd3251ea81b946d27c5da7096481f510
Rafael Willen <rafaelpadre20@gmail.com>
2021-12-10 01:14:21 +0100

    Criado navigation container

--- App.tsx
+++ App.tsx
@@ -6,8 +6,15 @@ import {
   Poppins_500Medium,
   Poppins_600SemiBold,
 } from "@expo-google-fonts/poppins";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Welcome from "./screens/Welcome";
+import Login from "./screens/Login";
+import CadastroCliente from "./screens/CadastroCliente";
+import CadastroProvedor from "./screens/CadastroProvedor";
+
+const AuthStack = createNativeStackNavigator();
 
 export default function App() {
   // Carrega as fontes
@@ -19,5 +26,17 @@ export default function App() {
 
   // Se as fontes não carregarem, apresenta a aplicação a carregar
   if (!fontsLoaded) return <AppLoading />;
-  return <Welcome />;
+  return (
+    <NavigationContainer>
+      <AuthStack.Navigator>
+        <AuthStack.Screen name="Welcome" component={Welcome} />
+        <AuthStack.Screen name="Login" component={Login} />
+        <AuthStack.Screen name="CadastroCliente" component={CadastroCliente} />
+        <AuthStack.Screen
+          name="CadastroProvedor"
+          component={CadastroProvedor}
+        />
+      </AuthStack.Navigator>
+    </NavigationContainer>
+  );
 }
