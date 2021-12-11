b82ebc032bcfc329096718ae3b72e61f5300ade1
Rafael Willen <rafaelpadre20@gmail.com>
2021-12-11 18:17:30 +0100

    Componetizado a navegação da autenticação

--- App.tsx
+++ App.tsx
@@ -7,14 +7,8 @@ import {
   Poppins_600SemiBold,
 } from "@expo-google-fonts/poppins";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Welcome from "./screens/Welcome";
-import Login from "./screens/Login";
-import CadastroCliente from "./screens/CadastroCliente";
-import CadastroProvedor from "./screens/CadastroProvedor";
-
-const AuthStack = createNativeStackNavigator();
+import AuthStackRoute from "./routes/AuthStackRoute";
 
 export default function App() {
   // Carrega as fontes
@@ -24,36 +18,11 @@ export default function App() {
     Poppins_600SemiBold,
   });
 
-  const screensHeaderOptions = {
-    headerShown: false,
-  };
-
   // Se as fontes não carregarem, apresenta a aplicação a carregar
   if (!fontsLoaded) return <AppLoading />;
   return (
     <NavigationContainer>
-      <AuthStack.Navigator>
-        <AuthStack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={screensHeaderOptions}
-        />
-        <AuthStack.Screen
-          name="Login"
-          component={Login}
-          options={screensHeaderOptions}
-        />
-        <AuthStack.Screen
-          name="CadastroCliente"
-          component={CadastroCliente}
-          options={screensHeaderOptions}
-        />
-        <AuthStack.Screen
-          name="CadastroProvedor"
-          component={CadastroProvedor}
-          options={screensHeaderOptions}
-        />
-      </AuthStack.Navigator>
+      <AuthStackRoute />
     </NavigationContainer>
   );
 }
---
+++ routes/AuthStackRoute.tsx
@@ -0,0 +1,40 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import CadastroCliente from "../screens/CadastroCliente";
+import CadastroProvedor from "../screens/CadastroProvedor";
+import Login from "../screens/Login";
+import Welcome from "../screens/Welcome";
+
+const AuthStack = createNativeStackNavigator();
+
+function AuthStackRoute() {
+  const screensHeaderOptions = {
+    headerShown: false,
+  };
+  return (
+    <AuthStack.Navigator>
+      <AuthStack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={screensHeaderOptions}
+      />
+      <AuthStack.Screen
+        name="Login"
+        component={Login}
+        options={screensHeaderOptions}
+      />
+      <AuthStack.Screen
+        name="CadastroCliente"
+        component={CadastroCliente}
+        options={screensHeaderOptions}
+      />
+      <AuthStack.Screen
+        name="CadastroProvedor"
+        component={CadastroProvedor}
+        options={screensHeaderOptions}
+      />
+    </AuthStack.Navigator>
+  );
+}
+
+export default AuthStackRoute;
