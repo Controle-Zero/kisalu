189a71a56c626e02361e8ce5cea6a16771f80aa2
Rafael Willen <rafaelpadre20@gmail.com>
2021-12-21 15:00:49 +0100

    Criado um mookup de login

---
+++ src/services/auth.ts
@@ -0,0 +1,13 @@
+export default function signIn() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        token: "r8u9eurvu9gwetgiaorjwkagh rf9o",
+        user: {
+          name: "Rafael Padre",
+          email: "rafaelpadre20@gmail.com",
+        },
+      });
+    }, 2000);
+  });
+}
