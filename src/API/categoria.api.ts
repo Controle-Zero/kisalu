becd0d47eed44b40370c05562eca5ad762fde266
regireis7 <regi@fisastrategy.com>
2021-12-30 03:06:55 +0100

    Cached categorias responses

--- src/API/categoria.api.ts
+++ src/API/categoria.api.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import NodeCache from "node-cache";
 
 dotenv.config();
 
+const cache = new NodeCache();
+
 export async function getCategorias() {
   const response = await axios.get(`${process.env.API_URL}/categoria`);
+  cache.set("categorias", response.data, 120);
   return response.data;
 }
--- src/services/categoria.services.ts
+++ src/services/categoria.services.ts
@@ -1,7 +1,14 @@
 import { getCategorias } from "../API/categoria.api";
 import Categoria from "../models/Categoria";
+import NodeCache from "node-cache";
+
+const categoriasCached = new NodeCache();
 
 export async function retornarCategorias(): Promise<Categoria[]> {
-  const categorias: Categoria[] = await getCategorias();
-  return categorias;
+  if (categoriasCached.get("categorias")) {
+    return categoriasCached.get("categorias")!!;
+  } else {
+    const categorias: Categoria[] = await getCategorias();
+    return categorias;
+  }
 }
