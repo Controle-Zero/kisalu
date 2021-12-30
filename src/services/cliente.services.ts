55a1358042f89f25b7efb819e5a253a93c53b782
regireis7 <regi@fisastrategy.com>
2021-12-30 03:51:16 +0100

    Cached cliente & provedor

--- src/services/cliente.services.ts
+++ src/services/cliente.services.ts
@@ -7,12 +7,13 @@ import {
 import Cliente from "../models/Cliente";
 import NodeCache from "node-cache";
 
-const tokenCache = new NodeCache();
+const cache = new NodeCache();
 
 export async function loginCliente(email: string, password: string) {
   const response = await getTokenCliente(email, password);
   if (response.status === 200) {
-    tokenCache.set("token_cliente", response.data.token, 600);
+    cache.set("token_cliente", response.data.token, 600);
+    cache.set("cliente", { email, password });
     return true;
   } else {
     return false;
@@ -23,7 +24,8 @@ export async function criarCliente(cliente: Cliente) {
   return await postCliente(cliente);
 }
 
-export async function retornarCliente(email: string): Promise<Cliente> {
+export async function retornarCliente(): Promise<Cliente> {
+  const { email } = cache.get("cliente")!!;
   const cliente: Cliente = await getCliente(email);
   return cliente;
 }
