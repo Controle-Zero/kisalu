a7101d16eca38b440e9d7682c49c3477b73c4dfb
Rafael Willen <rafaelpadre20@gmail.com>
2022-01-06 21:54:20 +0100

    Adicionado novos campos: Morada, IBAN e descrição

--- src/utils/validation/cadastroProvedorFormValidation.ts
+++ src/utils/validation/cadastroProvedorFormValidation.ts
@@ -1,11 +1,16 @@
 import * as yup from "yup";
 
+const regexBI = /^\d{9}[A-Z]{2}\d{3}$/g;
+const regexPhoneNumber = /^(\+2449\d{8})|(9\d{8})$/g;
+const regexAddress = /^(\w+\s?)+,\s?(\w+\s?)+(,\s?((\w+\s?)+))?$/g;
+const regexIban = /^AO06(\.\d{4}){5}\.\d$/g;
+
 export const cadastroProvedorSchema = yup.object({
   fullName: yup.string().required("Nome completo não deve estar vazio"),
   bi: yup
     .string()
     .required("BI não deve estar vazio")
-    .matches(/^\d{9}[A-Z]{2}\d{3}$/g, "Deve ser um BI válido"),
+    .matches(regexBI, "Deve ser um BI válido"),
   email: yup
     .string()
     .required("Email não deve estar vazio")
@@ -13,10 +18,7 @@ export const cadastroProvedorSchema = yup.object({
   phoneNumber: yup
     .string()
     .required("Nº de telefone não deve estar vazio")
-    .matches(
-      /^(\+2449\d{8})|(9\d{8})$/g,
-      "Deve ser um número de telefone de Angola"
-    ),
+    .matches(regexPhoneNumber, "Deve ser um número de telefone de Angola"),
   password: yup
     .string()
     .required("Password não pode estar vazia")
@@ -24,4 +26,16 @@ export const cadastroProvedorSchema = yup.object({
   passwordConfirmation: yup
     .string()
     .required("A confirmação não pode estar vazia"),
+  address: yup
+    .string()
+    .required("A morada não pode estar vazia")
+    .matches(
+      regexAddress,
+      "A morada deve ser do formato Província, Município, Rua"
+    ),
+  iban: yup
+    .string()
+    .required("O IBAN não pode estar vazio")
+    .matches(regexIban, "O IBAN não é válido"),
+  description: yup.string().required("A descrição não pode estar vazia"),
 });
