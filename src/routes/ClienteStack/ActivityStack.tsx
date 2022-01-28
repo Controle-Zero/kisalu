4ab3c64f38e39f00627f6d2bc443dd3d2e31eac7
Rafael Willen <rafaelpadre20@gmail.com>
2022-01-28 02:59:36 +0100

    feat: style the app bar of the activities screen

--- src/routes/ClienteStack/ActivityStack.tsx
+++ src/routes/ClienteStack/ActivityStack.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { FC } from "react";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Icon from "react-native-vector-icons/Ionicons";
+
 import Atividades from "../../screens/Client/Atividades";
 import { ActivityParamsList } from "../types/Cliente/ActivityParamsList";
+import { Colors } from "../../styles/appTheme";
+import Spacer from "../../components/layout/Spacer";
 
 const NativeActivityStack = createNativeStackNavigator<ActivityParamsList>();
 
@@ -12,10 +16,24 @@ function ActivityStack() {
       <NativeActivityStack.Screen
         name="AtividadesScreen"
         component={Atividades}
-        options={{ headerShown: false }}
+        options={{
+          headerTitle: "Atividades",
+          headerLeft: () => <HeaderLeft />,
+          contentStyle: {
+            borderTopColor: Colors.primary,
+            borderTopWidth: 1,
+          },
+        }}
       />
     </NativeActivityStack.Navigator>
   );
 }
+
+const HeaderLeft: FC = () => (
+  <>
+    <Icon name="list" size={35} color={Colors.secondary} />
+    <Spacer width={20} />
+  </>
+);
 
 export default ActivityStack;
