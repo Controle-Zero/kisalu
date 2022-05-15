e984a5aef35b7c6a5a74b6e3b56e13abdf748afe
Rafael Willen <rafaelpadre20@gmail.com>
2022-05-16 00:08:59 +0100

    🍱 add login bg image

--- src/styles/imageConstants.ts
+++ src/styles/imageConstants.ts
@@ -1,5 +1,6 @@
 import loginImage from "../assets/images/login-image.png";
 import appLogo from "../assets/images/logo.png";
 import defaultProfilePic from "../assets/images/default-profile-pic.png";
+import loginBackground from "../assets/images/login-bg.png";
 
-export { loginImage, appLogo, defaultProfilePic };
+export { loginImage, appLogo, defaultProfilePic, loginBackground };
