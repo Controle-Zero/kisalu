8586393721f5b62ef149cb2defbcbda6b30fdb84
Rafael Willen <rafaelpadre20@gmail.com>
2022-06-12 01:39:38 +0100

    ♻️ change formik to react hooks form

--- src/components/Forms/LoginForm/index.tsx
+++ src/components/Forms/LoginForm/index.tsx
@@ -8,79 +8,96 @@ import Spacer from "../../layout/Spacer";
 import TextField from "../../Input/TextField";
 import ErrorText from "../ErrorText";
 import Button from "../../Button";
-
-const initialValues: LoginFormValues = {
-  email: "",
-  password: "",
-  userType: "client",
-};
+import { Controller, useFormContext } from "react-hook-form";
+import { UserType } from "../../../contexts/AuthContext/types";
 
 const LoginForm: React.FC<Props> = ({ onSubmit }) => {
-  const [emailError, setEmailError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useFormContext<LoginFormValues>();
+
+  const [isClient, setIsClient] = useState(true);
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={loginSchema}
-    >
-      {({ handleChange, values, handleSubmit, errors, touched }) => {
-        const invalidEmail = (errors.email && touched.email) as boolean;
-        const invalidPassword = (errors.password &&
-          touched.password) as boolean;
-        setEmailError(invalidEmail);
-        setPasswordError(invalidPassword);
-        const isClient = values.userType === "client";
-        return (
-          <View>
-            <ToggleButtonRow
-              onValueChange={handleChange("userType")}
-              value={values.userType}
-            >
-              <ToggleButton
-                icon={() => (
-                  <ToggleButtonText isClient={isClient}>
-                    Cliente
-                  </ToggleButtonText>
-                )}
-                value="client"
-                isClient={!isClient}
-              />
-              <ToggleButton
-                icon={() => (
-                  <ToggleButtonText isClient={!isClient}>
-                    Provedor
-                  </ToggleButtonText>
-                )}
-                value="provider"
-                isClient={isClient}
-              />
-            </ToggleButtonRow>
-            <Spacer height={12} />
-            <TextField
-              label="Email"
-              onChangeText={handleChange("email")}
-              value={values.email}
-              hasError={emailError}
+    <View>
+      <Controller
+        control={control}
+        name="userType"
+        render={() => (
+          <ToggleButtonRow
+            onValueChange={(value) => {
+              const selectedUser = value as UserType;
+              setValue("userType", selectedUser);
+              if (selectedUser == "client" && !isClient)
+                setIsClient((value as UserType) == "client");
+              else if (selectedUser == "provider" && isClient)
+                setIsClient((value as UserType) == "client");
+            }}
+            value={getValues("userType")}
+          >
+            <ToggleButton
+              icon={() => (
+                <ToggleButtonText isClient={isClient}>Cliente</ToggleButtonText>
+              )}
+              value="client"
+              isClient={!isClient}
             />
-            {emailError && <ErrorText text={errors.email as string} />}
-            <Spacer height={12} />
-            <TextField
-              label="Password"
-              onChangeText={handleChange("password")}
-              value={values.password}
-              hasError={passwordError}
-              secureText
+            <ToggleButton
+              icon={() => (
+                <ToggleButtonText isClient={!isClient}>
+                  Provedor
+                </ToggleButtonText>
+              )}
+              value="provider"
+              isClient={isClient}
             />
-            {passwordError && <ErrorText text={errors.password as string} />}
-            <Spacer height={22} />
-            <Button text="Login" onPress={handleSubmit} />
-            <Spacer height={22} />
-          </View>
-        );
-      }}
-    </Formik>
+          </ToggleButtonRow>
+        )}
+      />
+      <Spacer height={12} />
+      <Controller
+        control={control}
+        name="email"
+        render={() => (
+          <TextField
+            label="Email"
+            onChangeText={(value) => setValue("email", value)}
+            value={getValues("email")}
+            hasError={!!errors.email}
+          />
+        )}
+      />
+      {errors.email && <ErrorText text={errors.email.message as string} />}
+      <Spacer height={12} />
+      <Controller
+        control={control}
+        name="password"
+        render={() => (
+          <TextField
+            label="Password"
+            onChangeText={(value) => setValue("password", value)}
+            value={getValues("password")}
+            hasError={!!errors.password}
+            secureText
+          />
+        )}
+      />
+      {errors.password && (
+        <ErrorText text={errors.password.message as string} />
+      )}
+      <Spacer height={22} />
+      <Button
+        text="Login"
+        onPress={handleSubmit<LoginFormValues>(onSubmit, (error) =>
+          console.error(error)
+        )}
+      />
+      <Spacer height={22} />
+    </View>
   );
 };
 
--- src/screens/auth/Login/index.tsx
+++ src/screens/auth/Login/index.tsx
@@ -1,19 +1,29 @@
-import { View } from "react-native";
 import React from "react";
 import { Container, Heading1, Heading2, Wrapper } from "./style";
 import { loginBackground } from "../../../styles/imageConstants";
 import LoginForm from "../../../components/Forms/LoginForm";
 import { handleLogin } from "./types";
 import useAuth from "../../../hooks/useAuth";
+import { useForm, FormProvider } from "react-hook-form";
+import { LoginFormValues } from "../../../components/Forms/LoginForm/types";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from "../../../components/Forms/LoginForm/loginFormValidation";
+
+const initialValues: LoginFormValues = {
+  email: "",
+  password: "",
+  userType: "client",
+};
 
 const Login = () => {
   const { signIn } = useAuth();
+  const formMethods = useForm({
+    defaultValues: initialValues,
+    resolver: yupResolver(loginSchema),
+  });
 
-  const handleLogin: handleLogin = async (
-    { email, password, userType },
-    actions
-  ) => {
-    actions.resetForm();
+  const handleLogin: handleLogin = async ({ email, password, userType }) => {
+    formMethods.reset();
     await signIn(email, password, userType);
   };
 
@@ -22,7 +32,9 @@ const Login = () => {
       <Wrapper>
         <Heading1>Bem-vindo de Volta</Heading1>
         <Heading2>Sentimos a sua falta</Heading2>
-        <LoginForm onSubmit={handleLogin} />
+        <FormProvider {...formMethods}>
+          <LoginForm onSubmit={handleLogin} />
+        </FormProvider>
       </Wrapper>
     </Container>
   );
